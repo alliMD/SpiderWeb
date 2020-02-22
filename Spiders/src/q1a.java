7ece4dcec21d81b8a8f0858b45cb825f01344284
alliMD <allison.mejiadiestra@mail.mcgill.ca>
2020-02-22 00:59:41 -0500

    create points in web, avoid overlap

--- Spiders/src/q1a.java
+++ Spiders/src/q1a.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Random;
+import java.lang.Math;
 
 public class q1a {
 	// Constants
 	public static final float Boundary = 1000.0f;
+	public static final float Margin = 0.2f; // threshold for comparing float values
 	
 	public static void main(String[] args) {
 		int n, t, k;
@@ -12,19 +15,27 @@ public class q1a {
 		k = Integer.parseInt(args[2]); //failure tolerance
 		
 		System.out.println("n= "+n+" t= "+t+" k="+k);
+		q1a q1a = new q1a();
+		q1a.execute(n, t, k);
+	}
+	
+	public void execute(int n, int t, int k) {
+		SpiderWeb web = new SpiderWeb(n, t, k);
+		web.CreatePoints();
 
 	}
 	
 	public class SpiderWeb {
-		ArrayList<Point> region;
+		ArrayList<Point> pointSet;
 		int totalPoints, totalThreads, failureTolerance;
+		Random rnd = new Random();
 		
 		SpiderWeb(int n, int t, int k){
 			this.totalPoints = n;
 			this.totalThreads = t;
 			this.failureTolerance = k;
 			
-			region = new ArrayList<Point>();
+			pointSet = new ArrayList<Point>();
 		}
 		
 		public void CreatePoints() {
@@ -33,12 +44,34 @@ public class q1a {
 			Point bottomLeft = new Point(0.0f, 0.0f);
 			Point topRight = new Point(Boundary, Boundary);
 			Point bottomRight = new Point(Boundary, 0.0f);
-			region.add(topLeft);
-			region.add(bottomLeft);
-			region.add(topRight);
-			region.add(bottomRight);
+			pointSet.add(topLeft);
+			pointSet.add(bottomLeft);
+			pointSet.add(topRight);
+			pointSet.add(bottomRight);
 			
 			// Randomly create the (n-4) remaining points
+			int remaining = totalPoints - 4;
+			while(remaining > 0) {
+				float randX = rnd.nextFloat() * Boundary;
+				float randY = rnd.nextFloat() * Boundary;
+				boolean overlap = false;
+				//check if overlaps with another point
+				for(Point point: pointSet) {
+					if(Math.abs(randX-point.x) < Margin && Math.abs(randY-point.y) < Margin) {
+						overlap = true;
+						break;
+					}
+				}
+				if(!overlap) {
+					Point pointCreated = new Point(randX, randY);
+					pointSet.add(pointCreated);
+					remaining--;
+				}
+			}
+			
+			for(Point point: pointSet) {
+				System.out.println("Point created = ("+point.x+", "+point.y+")");
+			}
 		}
 	}
 	
