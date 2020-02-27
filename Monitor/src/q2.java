be1b58ed21d05d9629e5bd74d36f0f0abaf265ad
alliMD <allison.mejiadiestra@mail.mcgill.ca>
2020-02-27 01:55:14 -0500

    added q2 monitor abstract class

---
+++ Monitor/src/q2.java
@@ -0,0 +1,64 @@
+
+public class q2 {
+	
+	public abstract class Monitor {
+		public abstract void enter();
+		public abstract void exit();
+		public abstract void await() throws InterruptedException;
+		public abstract void signal();
+	}
+
+	class MonitorSC extends Monitor{
+		public void enter() {
+			
+		}
+		
+		public void exit() {
+			
+		}
+		
+		public void await() throws InterruptedException{
+			
+		}
+		
+		public void signal() {
+			
+		}
+	}
+	
+	class MonitorSW extends Monitor{
+		public void enter() {
+			
+		}
+		
+		public void exit() {
+			
+		}
+		
+		public void await() throws InterruptedException{
+			
+		}
+		
+		public void signal() {
+			
+		}
+	}
+	
+	class MonitorSUW extends Monitor{
+		public void enter() {
+			
+		}
+		
+		public void exit() {
+			
+		}
+		
+		public void await() throws InterruptedException{
+			
+		}
+		
+		public void signal() {
+			
+		}
+	}
+}
